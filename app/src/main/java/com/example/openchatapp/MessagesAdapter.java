bf6de5bb68e0d9ec07b0c20470b8b7598a1b82eb
Michael Graves <mdgrav@protonmail.com>
2022-04-19 19:56:19 -0500

    Ability to chat added!

---
+++ app/src/main/java/com/example/openchatapp/MessagesAdapter.java
@@ -0,0 +1,94 @@
+package com.example.openchatapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+public class MessagesAdapter extends RecyclerView.Adapter {
+
+    Context context;
+    ArrayList<Messages> messagesArrayList;
+
+    int ITEM_SEND = 1;
+    int ITEM_RECEIVE = 2;
+
+    public MessagesAdapter(Context context, ArrayList<Messages> messagesArrayList) {
+        this.context = context;
+        this.messagesArrayList = messagesArrayList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == ITEM_SEND) {
+            View view = LayoutInflater.from(context).inflate(R.layout.senderchatlayout, parent, false);
+            return new SenderViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(context).inflate(R.layout.recieverchatlayout, parent, false);
+            return new ReceiverViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Messages messages = messagesArrayList.get(position);
+        if (holder.getClass() == SenderViewHolder.class) {
+            SenderViewHolder viewHolder = (SenderViewHolder) holder;
+            viewHolder.textViewMessage.setText(messages.getMessage());
+            viewHolder.timeOfMessage.setText(messages.getCurrentTime());
+        } else {
+            ReceiverViewHolder viewHolder = (ReceiverViewHolder) holder;
+            viewHolder.textViewMessage.setText(messages.getMessage());
+            viewHolder.timeOfMessage.setText(messages.getCurrentTime());
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Messages messages = messagesArrayList.get(position);
+
+        // If its the sender
+        if (FirebaseAuth.getInstance().getCurrentUser().getUid().equals(messages.getSenderID())) {
+            return ITEM_SEND;
+        } else {
+            return ITEM_RECEIVE; // If its the receiver
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return messagesArrayList.size();
+    }
+
+    class SenderViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewMessage;
+        TextView timeOfMessage;
+
+        public SenderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewMessage = itemView.findViewById(R.id.sendermessage);
+            timeOfMessage = itemView.findViewById(R.id.timeofmessage);
+        }
+    }
+
+    class ReceiverViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewMessage;
+        TextView timeOfMessage;
+
+        public ReceiverViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewMessage = itemView.findViewById(R.id.sendermessage);
+            timeOfMessage = itemView.findViewById(R.id.timeofmessage);
+        }
+    }
+}
--- app/src/main/java/com/example/openchatapp/SpecificChat.java
+++ app/src/main/java/com/example/openchatapp/SpecificChat.java
@@ -1,14 +1,186 @@
 package com.example.openchatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class SpecificChat extends AppCompatActivity {
+    EditText mGetMessage;
+    ImageButton mSendMessageButton;
+    CardView mSendMessageCardView;
+    androidx.appcompat.widget.Toolbar mToolBarOfSpecificChat;
+    ImageView mImageViewOfSpecificUser;
+    TextView mNameOfSpecificUser;
+
+    private String enteredMessage;
+    Intent intent;
+    String mRecieverName, senderName, mRecieverUID, mSenderUID;
+    private FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+
+    String senderRoom, recieverRoom;
+    ImageButton mBackButtonOfSpecificChat;
+
+    RecyclerView mMessageRecyclerView;
+    String currentTime;
+    Calendar calendar;
+    SimpleDateFormat simpleDateFormat;
+
+    MessagesAdapter messagesAdapter;
+    ArrayList<Messages> messagesArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specific_chat);
+
+        mGetMessage = findViewById(R.id.getmessage);
+        mSendMessageCardView = findViewById(R.id.cardviewofsendmessage);
+        mSendMessageButton = findViewById(R.id.imageviewofsendmessage);
+        mToolBarOfSpecificChat = findViewById(R.id.toolbarOfSpecificChat);
+        mNameOfSpecificUser = findViewById(R.id.nameofspecificuser);
+        mImageViewOfSpecificUser = findViewById(R.id.specificimageuserinimageview);
+        mBackButtonOfSpecificChat = findViewById(R.id.backButtonOfSpecificChat);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        calendar = Calendar.getInstance();
+        simpleDateFormat = new SimpleDateFormat("hh:mm a");
+
+        messagesArrayList = new ArrayList<>();
+        mMessageRecyclerView = findViewById(R.id.recyclerviewofspecificchat);
+
+        LinearLayoutManager linearLayoutManager= new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        mMessageRecyclerView.setLayoutManager(linearLayoutManager);
+        messagesAdapter = new MessagesAdapter(SpecificChat.this, messagesArrayList);
+        mMessageRecyclerView.setAdapter(messagesAdapter);
+
+        mSenderUID = firebaseAuth.getUid();
+        mRecieverUID = getIntent().getStringExtra("recieveruid");
+        mRecieverName = getIntent().getStringExtra("name");
+
+        senderRoom = mSenderUID + mRecieverUID;
+        recieverRoom = mRecieverUID + mSenderUID;
+
+        intent = getIntent();
+        setSupportActionBar(mToolBarOfSpecificChat);
+        mToolBarOfSpecificChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Toolbar Is Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child("chats").child(senderRoom).child("messages");
+        messagesAdapter = new MessagesAdapter(SpecificChat.this, messagesArrayList);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messagesArrayList.clear();
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    Messages messages = snapshot1.getValue(Messages.class);
+                    messagesArrayList.add(messages);
+                }
+                messagesAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // If back button is pressed, leave the chat
+        mBackButtonOfSpecificChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        mNameOfSpecificUser.setText(mRecieverName);
+        String uri = getIntent().getStringExtra("imageuri");
+        if (uri.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "NULL IS RECEIVED", Toast.LENGTH_SHORT).show();
+        } else {
+            Picasso.get().load(uri).into(mImageViewOfSpecificUser);
+        }
+
+        mSendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enteredMessage = mGetMessage.getText().toString();
+                if (enteredMessage.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter Message First", Toast.LENGTH_SHORT).show();
+                } else {
+                    Date date = new Date();
+                    currentTime = simpleDateFormat.format(calendar.getTime()); // Date for timestamp
+                    Messages messages = new Messages(enteredMessage, firebaseAuth.getUid(), date.getTime(), currentTime);
+                    firebaseDatabase = FirebaseDatabase.getInstance();
+                    firebaseDatabase.getReference().child("chats")
+                                .child(senderRoom)
+                                .child("messages")
+                                .push().setValue(messages).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            firebaseDatabase.getReference()
+                                    .child("chats")
+                                    .child(recieverRoom)
+                                    .child("messages")
+                                    .push()
+                                    .setValue(messages).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                }
+                            });
+                        }
+                    });
+                    mGetMessage.setText(null);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        messagesAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (messagesAdapter != null) {
+            messagesAdapter.notifyDataSetChanged();
+        }
     }
 }
